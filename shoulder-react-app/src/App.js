008e17c6920d4c8e7883c69421031cada1c65ccb
Marlon288 <71406913+Marlon288@users.noreply.github.com>
2023-03-07 22:07:19 +0000

    Added React Framework

---
+++ shoulder-react-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BoneModel from "./BoneModel";
+
+function App() {
+  return(
+    <>
+      <BoneModel/>
+      <button>Change Layer</button>
+      <button>Settings</button>
+    </>
+  );
+}
+
+export default App;
---
+++ shoulder-react-app/src/BoneModel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BoneModel() {
+  return (
+    <div>
+       HELLO WORLD 
+    </div>
+  )
+}
